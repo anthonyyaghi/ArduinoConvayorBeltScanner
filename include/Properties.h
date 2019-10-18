84fefd6f6deb083c2e45e758e2542d26cafbc667
anthonyyaghi <anthonyyaghi@outlook.com>
2019-10-18 15:06:36 +0300

    Add  include gard

--- include/Properties.h
+++ include/Properties.h
@@ -1,3 +1,6 @@
+#ifndef PROPERTIES_H
+#define PROPERTIES_H
+
 #define SENSOR_COUNT 5
 #define SENSOR_SPACING 30
 #define TRIG_PIN 11
@@ -8,3 +11,5 @@
 #define ADD_SIZE 5
 
 const int ADD_PINS[] = {1, 2, 3, 4, 5};
+
+#endif
